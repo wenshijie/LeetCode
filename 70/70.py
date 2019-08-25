d2cc49be34357866ebab8d725074cca35a80990a
wenshijie <276656491@qq.com>
2019-08-25 21:11:40 +0800

    70

--- 70/70.py
+++ 70/70.py
@@ -7,12 +7,15 @@ Created on Sun Aug 25 20:52:58 2019
 
 class Solution:
     def climbStairs(self, n: int) -> int:
+        result = [0]*(n+1)
         if n==1:
             return 1
-        elif n==2:
+        if n==2:
             return 2
-        else:
-            return self.climbStairs(n-1)+self.climbStairs(n-2)
-
+        result[1]=1
+        result[2]=2
+        for i in range(3,n+1):
+            result[i]=result[i-1]+result[i-2]
+        return result[n]
 S = Solution()
 print(S.climbStairs(38))
